be2fdabbd16d1bf87a42e52eca410be81629ec04
gahtanf <gahtanfadholii@gmail.com>
2023-11-18 15:45:42 +0700

    login

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -9,44 +9,40 @@
     
 </head>
     <style>
-        .gradient-custom {
-        /* fallback for old browsers */
-        background: #6a11cb;
-
-        /* Chrome 10-25, Safari 5.1-6 */
-        background: -webkit-linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
-
-        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-        background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))
+        .back{
+          background-image: url("img/Login.jpeg");
+          height: 100vh;
+          background-position: center;
+          background-size: cover;
         }
     </style>
 <body>
-    <form method="POST" action="{{ url('login') }}" class="px-50 gradient-custom">
+    <form method="POST" action="{{ url('login') }}" class="back">
         @csrf
         <div class="container py-5 h-100">
           <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="col-12 col-md-5 col-lg-6 col-xl-5">
+              <div class="card bg-D9D9D9 text-black" style="border-radius: 2rem; 
+              box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                 <div class="card-body p-5 text-center">
       
                   <div class="mb-md-5 mt-md-4 pb-5">
       
                     <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                    <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                    <p class="text-black-50 mb-5">Please enter your login and password!</p>
       
                     <div class="form-outline form-white mb-4">
                         <label class="form-label" for="typeEmailX"></label>
                         <input type="text" id="username" name="username" class="form-control form-control-lg" placeholder="Masukan Email" />
                     </div>
       
-                    <div class="form-outline form-white mb-4">
+                    <div class="form-outline form-white mb-5">
                         <label class="form-label" for="typePasswordX"></label>
                         <input type="password" id="password" name="password" class="form-control form-control-lg" placeholder="Masukan Password"/>
                     </div>
-      
-                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-      
-                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>      
+                   <div class="d-grid">
+                    <button type="button" class="btn btn-success btn-block" type="submit">Login</button>
+                  </div>
                   </div>      
                 </div>
               </div>
